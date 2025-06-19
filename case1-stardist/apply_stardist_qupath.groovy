89dd8b5d1d0a265b0974978405ed8377a036e596
Alan O'Callaghan <alan.ocallaghan@outlook.com>
2025-06-19 17:54:51 +0100

    Remove old files and update script

--- case1-stardist/apply_stardist_qupath.groovy
+++ case1-stardist/apply_stardist_qupath.groovy
@@ -2,16 +2,15 @@
  * Groovy script to apply Stardist using a model from bioimage.io.
  * For more information about StarDist + QuPath in general, see https://qupath.readthedocs.io/en/stable/docs/advanced/stardist.html
  *
- * Note that this is written for compatibility with QuPath v0.3.2... and is therefore rather awkward.
- * It requires separately installing two extensions:
+ * This is written for compatibility with QuPath v0.5.1 and qupath-extension-stardist v0.5.0
+ * It requires installing two extensions:
  *   - https://github.com/qupath/qupath-extension-stardist
- *   - https://github.com/qupath/qupath-extension-tensorflow
+ *   - https://github.com/qupath/qupath-extension-djl
  * It also only supports percentile normalization, with values that should be specified in the script 
  * (since the YAML is not parsed automatically).
  *
- * Future QuPath versions will reduce the complexity a bit by including more useful built-in methods.
- *
  * @author Pete Bankhead
+ * @author Alan O'Callaghan
  */
 
 
@@ -24,36 +23,52 @@ import qupath.opencv.tools.OpenCVTools
 setPixelSizeMicrons(0.5, 0.5)
 
 // specify the path to the model
-var pathModel = '/home/pape/Work/bioimageio/use-cases/case2/he-model-pretrained/bioimageio'
-// Define nput normalization percentiles etc
-double minPercentile = 1.0                       // Input normalization min percentile
-double maxPercentile = 99.8                      // Input normalization max percentile
+var pathModel = '/home/alan/Documents/github/imaging/use-cases/case1-stardist/stardist-hne-nuclei-segmentation_tensorflow_saved_model_bundle'
+
+String normalizationType = "percentile" // whether to use percentile normalization, alternatively "zeroMeanUnitVariance"
+double minPercentile = 1.0
+double maxPercentile = 99.8
 boolean jointChannelNormalize = true            // Optionally normalize channels together (rather than independently)
-double predictionThreshold = 0.5  // Prediction threshold
+double predictionThreshold = 0.5
 double pixelSizeMicrons = 0.25             // Specify if input resolution should be set (rather than using full resolution)
 boolean createAnnotations = true                // Create cells as annotations, which can then be edited (default is to create detection objects)
+boolean includeProbability = true         // include the detection probability as an object measurement
+double simplifyDistance = 0        // simplify distance threshold; set &le; 0 to turn off additional simplification
+int tileSize = 1024
+
+
+// define normalization parameters
+var normalizerBuilder = StarDist2D.imageNormalizationBuilder()
+    .maxDimension(4096)    // downsamples to ensure the image used to calculate normalization parameters are <= this value
+    // .downsample(4) // alternatively, directly specify the downsample to use
+    .useMask(true) // whether to mask the image using the object ROI when normalizing
+    .perChannel(!jointChannelNormalize) // if joint normalization, then don't do per-channel normalization
+
+if (normalizationType == "percentile") {
+    println "Using percentile normalization"
+    normalizerBuilder.percentiles(0.2, 99.8)  // Calculate image percentiles to use for normalization
+} else if (normalizationType == "zeroMeanUnitVariance") {
+    println "Using mean/variance normalization"
+    normalizerBuilder.zeroMeanUnitVariance()
+} else {
+    println "Unknown normalization type; will use the default..."
+}
 
 // Customize how StarDist will operate
 var stardistBuilder = StarDist2D.builder(pathModel)
         .threshold(predictionThreshold)
+        .includeProbability(includeProbability)
+        .simplify(simplifyDistance)
+        .tileSize(tileSize, tileSize)
 if (pixelSizeMicrons && Double.isFinite(pixelSizeMicrons))
     stardistBuilder.pixelSize(pixelSizeMicrons)
 
 if (createAnnotations)
     stardistBuilder.createAnnotations()
 
-if (jointChannelNormalize)
-    stardistBuilder.preprocess(
-            new NormalizePercentileJointOp(minPercentile, maxPercentile)
-    )
-else
-    stardistBuilder.normalizePercentiles(minPercentile, maxPercentile)
-
-// If necessary, customize tile size
-//stardistBuilder.tileSize(1024, 1024)
+stardistBuilder.preprocess(normalizerBuilder.build())
 
 var stardist = stardistBuilder.build()
-
 
 // Run detection for the selected objects
 var imageData = getCurrentImageData()
@@ -69,37 +84,6 @@ if (pathObjects.isEmpty()) {
     }
 }
 stardist.detectObjects(imageData, pathObjects)
+stardist.close()
 println 'Done!'
 
-
-
-/**
- * For QuPath v0.3.2, we need a custom ImageOp to perform joint channel normalization
- */
-class NormalizePercentileJointOp implements qupath.opencv.ops.ImageOp {
-
-    private double[] percentiles
-
-    NormalizePercentileJointOp(double percentileMin, double percentileMax) {
-        this.percentiles = [percentileMin, percentileMax] as double[]
-        if (percentileMin == percentileMax)
-            throw new IllegalArgumentException("Percentile min and max values cannot be identical!")
-    }
-
-    @Override
-    public Mat apply(Mat mat) {
-        // Need to reshape for percentiles to be correct in QuPath v0.3.2
-        var matTemp = mat.reshape(1, mat.rows()*mat.cols())
-        var range = OpenCVTools.percentiles(matTemp, percentiles)
-        double scale
-        if (range[1] == range[0]) {
-            println("Normalization percentiles give the same value ({}), scale will be Infinity", range[0])
-            scale = Double.POSITIVE_INFINITY
-        } else
-            scale = 1.0/(range[1] - range[0])
-        double offset = -range[0]
-        mat.convertTo(mat, mat.type(), scale, offset*scale)
-        return mat;
-    }
-
-}
